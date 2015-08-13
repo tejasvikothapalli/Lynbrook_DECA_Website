2bee0fc7bf80e61dc3fbbe5ae65272aa7fb3525d
tejasvikothapalli <tejasvi.kothapalli@gmail.com>
2015-08-12 18:00:35 -0700

    fixed forgot pass

--- js/forgot-pass.js
+++ js/forgot-pass.js
@@ -3,11 +3,18 @@ $("#forgot-pass-button").click(function(){
 
 	Parse.User.requestPasswordReset(email, {
   success: function() {
-  alert("success");
+  	$("#message").html("An email has been sent to your email address with instructions on how to change your password.");
+
+  	$("#message").css("color", "green");
+  	$(".forgot-pass").css("text-align", "center");
+  	$("#forgot-email").css("display", "none");
+  	$("#forgot-pass-button").css("display", "none");
   },
   error: function(error) {
     // Show the error message somewhere
-    alert("Error: " + error.code + " " + error.message);
+    $("#message").html("This email is not registered with us. Please try again!");
+  	$("#message").css("color", "red");
+  	$(".forgot-pass").css("text-align", "center");
   }
 });
 });
