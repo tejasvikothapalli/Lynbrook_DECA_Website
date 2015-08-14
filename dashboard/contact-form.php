6c1f011c7f83576eee28dbacfd6f5cf7e612fe6f
KarthickRavikumar <ravikumarkarthick@gmail.com>
2015-08-13 20:23:40 -0700

    just messing around with contact

--- dashboard/contact-form.php
+++ dashboard/contact-form.php
@@ -1,13 +1,16 @@
 <?php
-
+echo 'alert("REACHES HERE")';
 $name=$_POST['name'];
 $email=$_POST['email'];
 $message=$_POST['message'];
 
 $to="ravikumarkarthick@gmail.com";
 $subject="Message From Members";
-mail ($to,$subject,$message, "From: " . $name . "Email: " . $email);
-echo "Your Message has been sent!";
 
-
+$success=mail ($to,$subject,$message, "From: " . $name . "Email: " . $email);
+if ($success) {
+	echo 'alert("message successfully sent")';
+} else {
+	echo 'alert("message not successfully sent")';
+}
 ?>
--- dashboard/js/dashboard-parse.js
+++ dashboard/js/dashboard-parse.js
@@ -82,7 +82,7 @@ $("#save").click(function(){
 
 // Contact.html javascript
 
-$("#name").val(currentUser.get("firstname") + currentUser.get("lastname"));
+$("#name").val(currentUser.get("firstname") + " " + currentUser.get("lastname"));
 $("#email").val(currentUser.get("username"));
 
 
