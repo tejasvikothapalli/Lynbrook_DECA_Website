eb777b48cf28709f2d0953083d472206b6fc9793
Tejasvi Kothapalli <tejasvikothapalli@Tejasvis-MacBook-Pro.local>
2016-09-20 22:47:53 -0700

    fixed point log

--- dashboard/js/dashboard-parse.js
+++ dashboard/js/dashboard-parse.js
@@ -2,13 +2,13 @@
 Parse.initialize("DECA_APPLICATION_ID");
 Parse.serverURL = 'http://54.201.150.12:1337/parse';
 
-    	var currentUser = Parse.User.current();
-		if (currentUser) {
+var currentUser = Parse.User.current();
+if (currentUser) {
 		    // alert("you are logged in");
             $(".bodyDisplay").css("display", "block");
-		    $(".user-name").html(" "+currentUser.get("firstname")+ " " + currentUser.get("lastname") +" ");
+            $(".user-name").html(" "+currentUser.get("firstname")+ " " + currentUser.get("lastname") +" ");
 
-		} else {
+        } else {
 		    // alert("not logged in");
 		    window.location.href='../index.html';
 		}
@@ -19,7 +19,7 @@ Parse.serverURL = 'http://54.201.150.12:1337/parse';
             // if ($(window).width() > 1100)
             // {
             //     $(".nav-menu").css("display", "none");
-                
+
             // }
             if (screen.width < 790)
             {
@@ -28,10 +28,10 @@ Parse.serverURL = 'http://54.201.150.12:1337/parse';
             }
         });
 
-      
+
         
 
-$("#logout-button").click(function(){
+        $("#logout-button").click(function(){
 	// alert("working");
 
 	Parse.User.logOut();
@@ -44,44 +44,113 @@ $("#logout-button").click(function(){
 
 });
 
-var isopen = false;
-$("#icon-bar1").click(function()
-{
+        $("#pointsbutton").click(function(){
+            var thepass = 'lophiasing';
+            alert ($('#pointspass').val());
+            if ($('#pointspass').val() == thepass)
+            {
+            var currentUser = Parse.User.current();
+            var username = currentUser.get("username");
+            // alert(username);
 
-    if (isopen)
-    {
-        $(".nav-menu").css("display", "none");
-        
-        isopen = !isopen;
-    }
-    else {
-        $(".nav-menu").css("display", "block");
-        
-        isopen = !isopen;
-    }
-    
-});
+
+            var query = new Parse.Query("DECAdiamonds"); 
+
+            query.find( {
+                success: function listOfObjects(results) {
+                    var decaDiamonds = null;
+            // alert("Error");
+            // console.log("Successfully retrieved " + results.length);
+            // Do something with the returned Parse.Object values
+            if ( results.length == 0 ) {
+
+            } 
+            for (var i = 0; i < results.length; i++) { 
+               var object = results[i];
+               console.log(object.id + ' - ' + object.get('column'));
+               if(object.get('username') == username) {
+                decaDiamonds = object;
+                break;
+            }
+        }
+        if (decaDiamonds == null) {
+          var object = new Parse.Object("DECAdiamonds");
+          object.set( 'username', username ) ;
+          object.set( 'firstname', currentUser.get('firstname') ) ;
+          object.set( 'lastname', currentUser.get('lastname') ) ;
+          object.set( 'yearindeca', currentUser.get('yearindeca') ) ;
+          object.set( 'swag', 5 ) ;
+          object.save(null, {
+            success: function(object) {
+                alert("Saved new user");
+            },
+            error: function(object, error) {
+                alert('error saving for new user:' + error.message);
+            }
+        });
+                   // alert("fail");
+               } else {
+                decaDiamonds.set('swag', 5 ) ;
+                decaDiamonds.save(null, {
+                    success: function(object) {
+                        alert("Saved for existing user.");
+                    },
+                    error: function(object, error) {
+                        alert('Error saving for existing user:' + error.message);
+                    }
+                });
+            }
+
+
+        },
+        error: function (error) {
+           alert("Error")
+       }
+
+   })
+
+        }
+
+    }); // end of points button actions
+
+        var isopen = false;
+        $("#icon-bar1").click(function()
+        {
+
+            if (isopen)
+            {
+                $(".nav-menu").css("display", "none");
+
+                isopen = !isopen;
+            }
+            else {
+                $(".nav-menu").css("display", "block");
+
+                isopen = !isopen;
+            }
+
+        });
 
 //Profile.html Java Script
 currentUser.fetch({
   success: function(currentUser) {
     // The object was refreshed successfully.
-  },
-  error: function(currentUser, error) {
+},
+error: function(currentUser, error) {
     // The object was not refreshed successfully.
     // error is a Parse.Error with an error code and message.
-  }
+}
 });
 
 
 currentUser.fetch({
   success: function(currentUser) {
     // The object was refreshed successfully.
-  },
-  error: function(currentUser, error) {
+},
+error: function(currentUser, error) {
     // The object was not refreshed successfully.
     // error is a Parse.Error with an error code and message.
-  }
+}
 });
 currentUser.fetch();
 currentUser.fetch();
@@ -131,12 +200,12 @@ $("#save").click(function(){
 			currentUser.set("tshirtsize", tshirtsize);
 			currentUser.set("parentemail", parentemail);
 
-	currentUser.save();
-	alert("Profile Updated");
-	
+         currentUser.save();
+         alert("Profile Updated");
+
 // 	alert($("#firstname-profile").val() );
 // 	//currentUser.save();
-	
+
 //   currentUser.save(null, {
 //   success: function(currentUser) {
 //     // Execute any logic that should take place after the object is saved.
@@ -148,23 +217,23 @@ $("#save").click(function(){
 //     alert('Failed to create new object, with error code: ' + error.message);
 //   }
 // });
-	
+
 });
 
 
- function reset() {
+function reset() {
 	// alert("hello");
 	var currentUser = Parse.User.current();
 	var email = currentUser.get("username");
 
 	Parse.User.requestPasswordReset(email, {
-  success: function() {
-  alert("An email has been sent to your email address with instructions on how to change your password.");
-  },
-  error: function(error) {
+      success: function() {
+          alert("An email has been sent to your email address with instructions on how to change your password.");
+      },
+      error: function(error) {
     // Show the error message somewhere
     alert("Error: " + error.code + " " + error.message);
-  }
+}
 });
 }
 
@@ -176,143 +245,143 @@ $("#email").val(currentUser.get("username"));
 
 
 function harlem(){
-                function c() {
-                    var e = document.createElement("link");
-                    e.setAttribute("type", "text/css");
-                    e.setAttribute("rel", "stylesheet");
-                    e.setAttribute("href", f);
-                    e.setAttribute("class", l);
-                    document.body.appendChild(e)
+    function c() {
+        var e = document.createElement("link");
+        e.setAttribute("type", "text/css");
+        e.setAttribute("rel", "stylesheet");
+        e.setAttribute("href", f);
+        e.setAttribute("class", l);
+        document.body.appendChild(e)
+    }
+    function h() {
+        var e = document.getElementsByClassName(l);
+        for (var t = 0; t < e.length; t++) {
+            document.body.removeChild(e[t])
+        }
+    }
+    function p() {
+        var e = document.createElement("div");
+        e.setAttribute("class", a);
+        document.body.appendChild(e);
+        setTimeout(function () {
+            document.body.removeChild(e)
+        }, 100)
+    }
+    function d(e) {
+        return {
+            height: e.offsetHeight,
+            width: e.offsetWidth
+        }
+    }
+    function v(i) {
+        return true
+    }
+    function m(e) {
+        var t = e;
+        var n = 0;
+        while ( !! t) {
+            n += t.offsetTop;
+            t = t.offsetParent
+        }
+        return n
+    }
+    function g() {
+        var e = document.documentElement;
+        if ( !! window.innerWidth) {
+            return window.innerHeight
+        } else if (e && !isNaN(e.clientHeight)) {
+            return e.clientHeight
+        }
+        return 0
+    }
+    function y() {
+        if (window.pageYOffset) {
+            return window.pageYOffset
+        }
+        return Math.max(document.documentElement.scrollTop, document.body.scrollTop)
+    }
+    function E(e) {
+        var t = m(e);
+        return t >= w && t <= b + w
+    }
+    function S() {
+        var e = document.createElement("audio");
+        e.setAttribute("class", l);
+        e.src = i;
+        e.loop = false;
+        e.addEventListener("canplay", function () {
+            setTimeout(function () {
+                x(k)
+            }, 500);
+            setTimeout(function () {
+                N();
+                p();
+                for (var e = 0; e < O.length; e++) {
+                    T(O[e])
                 }
-                function h() {
-                    var e = document.getElementsByClassName(l);
-                    for (var t = 0; t < e.length; t++) {
-                        document.body.removeChild(e[t])
-                    }
-                }
-                function p() {
-                    var e = document.createElement("div");
-                    e.setAttribute("class", a);
-                    document.body.appendChild(e);
-                    setTimeout(function () {
-                        document.body.removeChild(e)
-                    }, 100)
-                }
-                function d(e) {
-                    return {
-                        height: e.offsetHeight,
-                        width: e.offsetWidth
-                    }
-                }
-                function v(i) {
-                    return true
-                }
-                function m(e) {
-                    var t = e;
-                    var n = 0;
-                    while ( !! t) {
-                        n += t.offsetTop;
-                        t = t.offsetParent
-                    }
-                    return n
-                }
-                function g() {
-                    var e = document.documentElement;
-                    if ( !! window.innerWidth) {
-                        return window.innerHeight
-                    } else if (e && !isNaN(e.clientHeight)) {
-                        return e.clientHeight
-                    }
-                    return 0
-                }
-                function y() {
-                    if (window.pageYOffset) {
-                        return window.pageYOffset
-                    }
-                    return Math.max(document.documentElement.scrollTop, document.body.scrollTop)
-                }
-                function E(e) {
-                    var t = m(e);
-                    return t >= w && t <= b + w
-                }
-                function S() {
-                    var e = document.createElement("audio");
-                    e.setAttribute("class", l);
-                    e.src = i;
-                    e.loop = false;
-                    e.addEventListener("canplay", function () {
-                        setTimeout(function () {
-                            x(k)
-                        }, 500);
-                        setTimeout(function () {
-                            N();
-                            p();
-                            for (var e = 0; e < O.length; e++) {
-                                T(O[e])
-                            }
-                        }, 15500)
-                    }, true);
-                    e.addEventListener("ended", function () {
-                        N();
-                        h()
-                    }, true);
-                    e.innerHTML = " <p>If you are reading this, it is because your browser does not support the audio element. We recommend that you get a new browser.</p> <p>";
-                    document.body.appendChild(e);
-                    e.play()
-                }
-                function x(e) {
-                    e.className += " " + s + " " + o
-                }
-                function T(e) {
-                    e.className += " " + s + " " + u[Math.floor(Math.random() * u.length)]
-                }
-                function N() {
-                    var e = document.getElementsByClassName(s);
-                    var t = new RegExp("\\b" + s + "\\b");
-                    for (var n = 0; n < e.length;) {
-                        e[n].className = e[n].className.replace(t, "")
-                    }
-                }
-                var e = 30;
-                var t = 30;
-                var n = 350;
-                var r = 350;
-                var i = "//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake.mp3";
-                var s = "mw-harlem_shake_me";
-                var o = "im_first";
-                var u = ["im_drunk", "im_baked", "im_trippin", "im_blown"];
-                var a = "mw-strobe_light";
-                var f = "//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake-style.css";
-                var l = "mw_added_css";
-                var b = g();
-                var w = y();
-                var C = document.getElementsByTagName("*");
-                var k = null;
-                for (var L = 0; L < C.length; L++) {
-                    var A = C[L];
+            }, 15500)
+        }, true);
+        e.addEventListener("ended", function () {
+            N();
+            h()
+        }, true);
+        e.innerHTML = " <p>If you are reading this, it is because your browser does not support the audio element. We recommend that you get a new browser.</p> <p>";
+        document.body.appendChild(e);
+        e.play()
+    }
+    function x(e) {
+        e.className += " " + s + " " + o
+    }
+    function T(e) {
+        e.className += " " + s + " " + u[Math.floor(Math.random() * u.length)]
+    }
+    function N() {
+        var e = document.getElementsByClassName(s);
+        var t = new RegExp("\\b" + s + "\\b");
+        for (var n = 0; n < e.length;) {
+            e[n].className = e[n].className.replace(t, "")
+        }
+    }
+    var e = 30;
+    var t = 30;
+    var n = 350;
+    var r = 350;
+    var i = "//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake.mp3";
+    var s = "mw-harlem_shake_me";
+    var o = "im_first";
+    var u = ["im_drunk", "im_baked", "im_trippin", "im_blown"];
+    var a = "mw-strobe_light";
+    var f = "//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake-style.css";
+    var l = "mw_added_css";
+    var b = g();
+    var w = y();
+    var C = document.getElementsByTagName("*");
+    var k = null;
+    for (var L = 0; L < C.length; L++) {
+        var A = C[L];
 
-                    var s2 = d(A);
+        var s2 = d(A);
 
-                    if (s2.height > e && s2.height < n && s2.width > t && s2.width < r) {
-                        if (E(A)) {
-                            k = A;
-                            break
-                        }
-                    }
-                }
-                if (A === null) {
-                    console.warn("Could not find a node of the right size. Please try a different page.");
-                    return
-                }
-                c();
-                S();
-                var O = [];
-                for (var L = 0; L < C.length; L++) {
-                    var A = C[L];
-                    if (v(A)) {
-                        O.push(A)
-                    }
-                }
+        if (s2.height > e && s2.height < n && s2.width > t && s2.width < r) {
+            if (E(A)) {
+                k = A;
+                break
             }
+        }
+    }
+    if (A === null) {
+        console.warn("Could not find a node of the right size. Please try a different page.");
+        return
+    }
+    c();
+    S();
+    var O = [];
+    for (var L = 0; L < C.length; L++) {
+        var A = C[L];
+        if (v(A)) {
+            O.push(A)
+        }
+    }
+}
 
 
