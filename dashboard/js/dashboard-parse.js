3e9b1a833f1654823fe58fa77e88bdb2522dabf3
Tejasvi Kothapalli <tejasvikothapalli@Tejasvis-MacBook-Pro.local>
2016-10-19 13:27:16 -0700

    ashwin

--- dashboard/js/dashboard-parse.js
+++ dashboard/js/dashboard-parse.js
@@ -164,7 +164,7 @@ if (currentUser) {
    });
 
             $("#pointsbutton").click(function(){
-            var thepass = 'pholyfe';                 						// CHANGE THIS
+            var thepass = 'bedbug';                 						// CHANGE THIS
             // alert ($('#pointspass').val());
             if ($('#pointspass').val() == thepass)
             {
@@ -199,7 +199,7 @@ if (currentUser) {
           object.set( 'firstname', currentUser.get('firstname') ) ;
           object.set( 'lastname', currentUser.get('lastname') ) ;
           object.set( 'yearindeca', currentUser.get('yearindeca') ) ;
-          object.set( 'FM_101416', 3 ) ;									 // CHANGE THIS
+          object.set( 'SS_101916', 3 ) ;									 // CHANGE THIS
           object.save(null, {
             success: function(object) {
                 // alert("Saved new user");
@@ -211,7 +211,7 @@ if (currentUser) {
         });
                    // alert("fail");
                } else {
-                decaDiamonds.set( 'FM_101416', 3 ) ;							 // CHANGE THIS
+                decaDiamonds.set( 'SS_101916', 3 ) ;							 // CHANGE THIS
                 decaDiamonds.save(null, {
                     success: function(object) {
                         // alert("Saved for existing user.");
