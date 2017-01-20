85a86a49473c2d39f24ad43478a6834dfa092ff7
tejasvikothapalli <tejasvi.kothapalli@gmail.com>
2017-01-20 00:35:01 -0800

    Update dashboard-parse.js

--- dashboard/js/dashboard-parse.js
+++ dashboard/js/dashboard-parse.js
@@ -178,7 +178,7 @@ if (currentUser) {
 
 
             $("#pointsbutton").click(function(){
-            var thepass = 'review';                 						// CHANGE THIS
+            var thepass = 'formsdue';                 						// CHANGE THIS
             // alert ($('#pointspass').val());
             if ($('#pointspass').val() == thepass)
             {
@@ -213,7 +213,7 @@ if (currentUser) {
             	object.set( 'firstname', currentUser.get('firstname') ) ;
             	object.set( 'lastname', currentUser.get('lastname') ) ;
             	object.set( 'yearindeca', currentUser.get('yearindeca') ) ;
-          object.set( 'SS_011817', 3 ) ;									 // CHANGE THIS
+          object.set( 'FM_012017', 3 ) ;									 // CHANGE THIS
           object.save(null, {
           	success: function(object) {
                 // alert("Saved new user");
@@ -226,7 +226,7 @@ if (currentUser) {
         });
                    // alert("fail");
                } else {
-                decaDiamonds.set( 'SS_011817', 3 ) ;							 // CHANGE THIS
+                decaDiamonds.set( 'FM_012017', 3 ) ;							 // CHANGE THIS
                 decaDiamonds.save(null, {
                 	success: function(object) {
                         // alert("Saved for existing user.");
